0236f2b4efd78a66d1b7b015e80a6bc31911aa7a
Christian Fritsch <chr.fritsch@gmx.net>
2021-11-30 15:24:01 +0100

    Increase min drush version (#24)
    * Increase min drush version
    
    * Update composer.json
    
    * test: add action
    
    * empty
    
    * empty
    
    * empty
    
    * empty

--- scripts/travis/01-setup-environment.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-export PATH="`echo $(pwd)`/vendor/bin:$HOME/.composer/vendor/bin:$PATH"
--- scripts/travis/02-install-thunder.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-composer install
-
-cd docroot
-/usr/bin/env PHP_OPTIONS="-d sendmail_path=`which true`" drush si thunder --db-url=mysql://travis@127.0.0.1/thunder -y
